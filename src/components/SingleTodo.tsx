71b2c50a337d62a12011cb24b613191b5786ca22
Tahsin Karim <tahsinkarim.dev@gmail.com>
2023-02-07 23:23:44 +0600

    updated style css

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -43,9 +43,9 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos, index }) => {
   }, [edit]);
   return (
     <Draggable draggableId={todo.id.toString()} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <form
-          className='todos__single'
+          className={`todos__single ${snapshot.isDragging ? "drag" : ""}`}
           onSubmit={(e) => handleEdit(e, todo.id)}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -20,9 +20,9 @@ const TodoList: React.FC<Props> = ({
   return (
     <div className='container'>
       <Droppable droppableId='TodosList'>
-        {(provided) => (
+        {(provided, snapshot) => (
           <div
-            className='todos'
+            className={`todos ${snapshot.isDraggingOver ? "dragactive" : ""}`}
             ref={provided.innerRef}
             {...provided.droppableProps}
           >
@@ -36,13 +36,16 @@ const TodoList: React.FC<Props> = ({
                 setTodos={setTodos}
               />
             ))}
+            {provided.placeholder}
           </div>
         )}
       </Droppable>
       <Droppable droppableId='TodosRemove'>
-        {(provided) => (
+        {(provided, snapshot) => (
           <div
-            className='todos remove'
+            className={`todos remove ${
+              snapshot.isDraggingOver ? "dragcomplete" : ""
+            }`}
             ref={provided.innerRef}
             {...provided.droppableProps}
           >
